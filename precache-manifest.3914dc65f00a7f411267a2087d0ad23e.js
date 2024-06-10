0bb625ab0bbacf6d5d7592537b0fc0b7911fa734
sulaimanalamsyah <102284555+sulaimanalamsyah@users.noreply.github.com>
2024-06-10 14:26:36 +0700

    Initial commit

---
+++ precache-manifest.3914dc65f00a7f411267a2087d0ad23e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "963859328b6bd31e8651",
+    "url": "/sds_upload/static/css/main.ef0ed4d1.chunk.css"
+  },
+  {
+    "revision": "963859328b6bd31e8651",
+    "url": "/sds_upload/static/js/main.96385932.chunk.js"
+  },
+  {
+    "revision": "488c458b3fb97a74792a",
+    "url": "/sds_upload/static/js/1.488c458b.chunk.js"
+  },
+  {
+    "revision": "ef009cb7909c56b5070c",
+    "url": "/sds_upload/static/css/2.3dfaf186.chunk.css"
+  },
+  {
+    "revision": "ef009cb7909c56b5070c",
+    "url": "/sds_upload/static/js/2.ef009cb7.chunk.js"
+  },
+  {
+    "revision": "0ba2a0530f4601ff8097",
+    "url": "/sds_upload/static/js/runtime~main.0ba2a053.js"
+  },
+  {
+    "revision": "a2ac7db5bdf62cec15165f2efa42bf5a",
+    "url": "/sds_upload/static/media/image1.a2ac7db5.jpg"
+  },
+  {
+    "revision": "39d3aac90c2302fe490c014e6c937e7d",
+    "url": "/sds_upload/static/media/image2.39d3aac9.jpg"
+  },
+  {
+    "revision": "d3aa81e45bfd4b1bcd5228a6bea3e2de",
+    "url": "/sds_upload/static/media/image3.d3aa81e4.jpg"
+  },
+  {
+    "revision": "39d3aac90c2302fe490c014e6c937e7d",
+    "url": "/sds_upload/static/media/image1.39d3aac9.jpg"
+  },
+  {
+    "revision": "9635cb93419aacbbc2b7c81f0c7d2a3a",
+    "url": "/sds_upload/static/media/image2.9635cb93.jpg"
+  },
+  {
+    "revision": "4584c2da8c4111ac0246f0fd9ce8c6c9",
+    "url": "/sds_upload/static/media/image3.4584c2da.jpg"
+  },
+  {
+    "revision": "0d52f69d715092f2c9b2f8bc5199e3af",
+    "url": "/sds_upload/static/media/main.0d52f69d.jpg"
+  },
+  {
+    "revision": "b2984ed776244a1f56fe53b02b60ad8d",
+    "url": "/sds_upload/index.html"
+  }
+];
